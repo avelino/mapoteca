5f371da3d74ad665d331c0b07014edf5166755a1
Matheus Marsiglio <matmarsiglio@gmail.com>
2020-03-31 18:04:52 -0300

    chore(env var): add master pub key env var

--- config/config.go
+++ config/config.go
@@ -18,8 +18,19 @@ type serverConfig struct {
 	Port string
 }
 
+type yubicoConfig struct {
+	ClientId string
+	ApiKey   string
+}
+
+type adminConfig struct {
+	MasterPublicKey string
+}
+
 var DatabaseConfig databaseConfig
 var ServerConfig serverConfig
+var YubicoConfig yubicoConfig
+var AdminConfig adminConfig
 
 func Init() {
 	var err = godotenv.Load()
@@ -39,5 +50,14 @@ func Init() {
 
 	ServerConfig = serverConfig{
 		Port: os.Getenv("SERVER_PORT"),
+	}
+
+	YubicoConfig = yubicoConfig{
+		ClientId: os.Getenv("YUBICO_CLIENT_ID"),
+		ApiKey:   os.Getenv("YUBICO_API_KEY"),
+	}
+
+	AdminConfig = adminConfig{
+		MasterPublicKey: os.Getenv("ADMIN_MASTER_PUBLIC_KEY"),
 	}
 }
